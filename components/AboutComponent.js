cb954feb055c86f7e5dcdc2de316cedb6b73e12a
Gwen S-Hill <gsmithhill@gmail.com>
2021-04-15 19:30:35 -0400

    Setting up Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -7,9 +7,22 @@ import { PARTNERS } from '../shared/partners';
 
 
 
-/*function Mission() {
+function Mission() {
+    return (
+        <Card title='Our Mission'>
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods 
+                and backcountry of the World Wide Web Wilderness. We increase access 
+                to adventure for the public while promoting safe and respectful use of 
+                resources. The expert wilderness trekkers on our staff personally verify 
+                each campsite to make sure that they are up to our standards. We also 
+                present a platform for campers to share reviews on campsites they have 
+                visited with each other.
+            </Text>
+        </Card>
+    )
 }
-*/
+
 
 class About extends Component {
     
@@ -36,18 +49,7 @@ class About extends Component {
         };
         return (
             <ScrollView>
-                <Card title='Our Mission'>
-                    <Text style={{margin: 10}}>
-                        We present a curated database of the best campsites in the vast woods 
-                        and backcountry of the World Wide Web Wilderness. We increase access 
-                        to adventure for the public while promoting safe and respectful use of 
-                        resources. The expert wilderness trekkers on our staff personally verify 
-                        each campsite to make sure that they are up to our standards. We also 
-                        present a platform for campers to share reviews on campsites they have 
-                        visited with each other.
-                    </Text>
-                </Card>
-
+                <Mission />
                 <Card title='Community Partners'>
                     <FlatList
                     data={this.state.partners}
